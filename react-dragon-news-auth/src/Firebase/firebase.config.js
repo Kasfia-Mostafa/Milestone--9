4dac384e695b99daf4399d3e42dc8e3886337ee4
SwornaCode <kasfiamostafa03@gmail.com>
2023-10-05 09:58:42 +0600

    install

--- react-dragon-news-auth/src/Firebase/firebase.config.js
+++ react-dragon-news-auth/src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAYtIhe0GhbPS9RXCku7eZxOJNxHKxHYUg",
-  authDomain: "react-dragon-news-auth-26688.firebaseapp.com",
-  projectId: "react-dragon-news-auth-26688",
-  storageBucket: "react-dragon-news-auth-26688.appspot.com",
-  messagingSenderId: "496414893869",
-  appId: "1:496414893869:web:9d94719f87bcb2bdf7d5a4"
+  apiKey: "AIzaSyD5t8sldRyoEzuicQC5GxlqUtopigFDrWY",
+  authDomain: "react-dragon-news-auth.firebaseapp.com",
+  projectId: "react-dragon-news-auth",
+  storageBucket: "react-dragon-news-auth.appspot.com",
+  messagingSenderId: "750044483429",
+  appId: "1:750044483429:web:77f4f5d3f33234b21f5b8c"
 };
 
 // Initialize Firebase
