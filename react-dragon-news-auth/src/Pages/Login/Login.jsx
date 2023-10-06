52d121298522fe8f188c3ff2bfdcafbeca33d25c
SwornaCode <kasfiamostafa03@gmail.com>
2023-10-06 15:40:39 +0600

    firebase secure

--- react-dragon-news-auth/src/Pages/Login/Login.jsx
+++ react-dragon-news-auth/src/Pages/Login/Login.jsx
@@ -1,10 +1,80 @@
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
+import { useContext } from "react";
+import { AuthContext } from "../../Providers/AuthProviders";
 
 const Login = () => {
+  const { signIn } = useContext(AuthContext);
+  const location = useLocation;
+  const navigate = useNavigate();
+
+  console.log("Location in the login page", location);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(e.currentTarget);
+    const form = new FormData(e.currentTarget);
+    const email = form.get("email");
+    const password = form.get("password");
+    console.log(email, password);
+
+    signIn()
+      .then((result) => {
+        console.log(result.user);
+        navigate(location?.state ? location.state : "/");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <div>
       <Navbar></Navbar>
-      <h2 className="text-3xl">This is login</h2>
+      <div>
+        <h2 className="text-3xl my-10 text-center">Please login</h2>
+
+        <form onSubmit={handleLogin} className="md:w-3/4 lg:w-1/2 mx-auto">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </form>
+        <p className="text-center">
+          Do not have an account ?{" "}
+          <Link to="/register" className="text-blue-600 font-bold">
+            Register
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
